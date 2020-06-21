4933628fa4e5fa542e44f6b25b68c425361897a8
Luis <lfegea@aol.com>
2020-06-21 09:53:03 -0700

    Updates

---
+++ precache-manifest.1a25ed01eec88504073b1be647115b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbb0b23fd8ee460ff7a4",
+    "url": "/ipfs-nft/static/css/main.ed57285a.chunk.css"
+  },
+  {
+    "revision": "cbb0b23fd8ee460ff7a4",
+    "url": "/ipfs-nft/static/js/main.cbb0b23f.chunk.js"
+  },
+  {
+    "revision": "7a9c4fc1f7c42d9ba827",
+    "url": "/ipfs-nft/static/css/1.6b47bcc0.chunk.css"
+  },
+  {
+    "revision": "7a9c4fc1f7c42d9ba827",
+    "url": "/ipfs-nft/static/js/1.7a9c4fc1.chunk.js"
+  },
+  {
+    "revision": "42f59e0e092d3dfbdf03",
+    "url": "/ipfs-nft/static/js/runtime~main.42f59e0e.js"
+  },
+  {
+    "revision": "2cb6ff22d96bffab2542223d811a3407",
+    "url": "/ipfs-nft/index.html"
+  }
+];
